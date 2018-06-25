d67976e30e180d08c6d2feb5b32c37c3e8a89f6c
massa <massafranck@gmail.com>
2018-06-25 18:37:56 +0200

    corrigé

---
+++ assets/js/fichier.js
@@ -0,0 +1,6 @@
+///declaration de la fonction qui permet de vider les champs du formulaire
+function raz(){
+  document.getElementById('lastname').value = '';
+  document.getElementById('firstname').value = '';
+  document.getElementById('city').value = '';
+}
